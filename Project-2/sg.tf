2058fad5ca601fdcf0a25657cf05c695d61499ab
Israel Tetteh <Tettehisrael1@gmail.com>
2023-08-10 18:28:21 +0100

    sg

---
+++ Project-2/sg.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "jenkins-sg" {
+  vpc_id = aws_vpc.jenkins-vpc.id
+
+  ingress {
+    description = "incoming SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # to be able ot access the EC2 instance on port 8080
+  ingress {
+    description = "incoming HTTP traffic"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "incoming 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Jenkins EC2 instance to being able to talk to the internet
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
+  }
+}
