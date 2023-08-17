420d3ae37374779cdd053495ca19a43128b57b5c
Israel Tetteh <Tettehisrael1@gmail.com>
2023-08-17 14:16:31 +0100

    provider

---
+++ Project-4/Terraform/provider.tf
@@ -0,0 +1,16 @@
+
+# https://cloud.google.com/docs/terraform/best-practices-for-terraform
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.27.0"
+    }
+  }
+
+  required_version = "~> 1.0"
+}
